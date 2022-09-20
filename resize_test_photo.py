6a6f870dd0b3b85edb69f07a027a58549ac78a07
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2022-09-20 12:36:45 +0900

    [add] 入力写真のサイズを下げるプログラムの追加

---
+++ resize_test_photo.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+import os
+import glob
+from PIL import Image
+
+os.makedirs('./assets/test',exist_ok=True)
+file = './assets/test/IMG_7007.jpg'
+
+# 画像の読み込み(RGB)
+img = cv2.imread(file)
+print(img.shape)
+x, y, z = img.shape
+print(f"x: {x}, y: {y}, z: {z}")
+resize_img = cv2.resize(img, dsize = None, fx = 0.25, fy = 0.25)
+print(resize_img.shape)
+name = file.split('/')[-1]
+name = name.split('.')[0] 
+print(name)
+cv2.imwrite(f"./assets/test/{name}_r.jpg", resize_img)
