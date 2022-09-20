2c7e5394cfb3362ac568db411830e9ba8a63e158
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2022-09-20 12:43:07 +0900

    [add] test photo

--- resize_test_photo.py
+++ resize_test_photo.py
@@ -5,16 +5,17 @@ import glob
 from PIL import Image
 
 os.makedirs('./assets/test',exist_ok=True)
-file = './assets/test/IMG_7007.jpg'
+file = files = glob.glob('./assets/test/IMG_*.jpg')
 
+for i, file in enumerate(files):
 # 画像の読み込み(RGB)
-img = cv2.imread(file)
-print(img.shape)
-x, y, z = img.shape
-print(f"x: {x}, y: {y}, z: {z}")
-resize_img = cv2.resize(img, dsize = None, fx = 0.25, fy = 0.25)
-print(resize_img.shape)
-name = file.split('/')[-1]
-name = name.split('.')[0] 
-print(name)
-cv2.imwrite(f"./assets/test/{name}_r.jpg", resize_img)
+  img = cv2.imread(file)
+  print(img.shape)
+  x, y, z = img.shape
+  print(f"x: {x}, y: {y}, z: {z}")
+  resize_img = cv2.resize(img, dsize = None, fx = 0.25, fy = 0.25)
+  print(resize_img.shape)
+  name = file.split('/')[-1]
+  name = name.split('.')[0] 
+  print(name)
+  cv2.imwrite(f"./assets/test/{name}_r.jpg", resize_img)
